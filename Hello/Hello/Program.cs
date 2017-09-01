2218d84b334a3876466a4c74b3608b4dd15ab5e7
surachai-p <surachai.pi@kmitl.ac.th>
2017-09-01 11:24:30 +0700

    B1-1

--- Hello/Hello/Program.cs
+++ Hello/Hello/Program.cs
@@ -11,6 +11,7 @@ namespace Hello
         static void Main(string[] args)
         {
             Console.WriteLine("Hello computer lab B1");
+            Console.WriteLine("Hello computer lab B1-1");
             Console.ReadKey();
         }
     }
